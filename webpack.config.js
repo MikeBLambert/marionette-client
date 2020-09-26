029655c615f8ffdbd08f727449633b1cfac07cec
mlamb <mlamb128@gmail.com>
2020-09-26 10:36:58 -0700

    update tsconfig to and webpack to use ts only

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const webpackEnv = process.env.NODE_ENV || 'development';
 module.exports = {
   mode: webpackEnv,
   entry: {
-    app: path.join(rootDir, './index.web.js'),
+    app: path.join(rootDir, './index.web.ts'),
   },
   output: {
     path: path.resolve(rootDir, 'dist'),
